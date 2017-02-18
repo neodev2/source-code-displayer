d6d3af88e0c73d8d22b241a97d0ec2f4570193bb
XXX <XXX>
2017-02-18 21:09:11 +0100

    XXX

--- public/resources/js/client.js
+++ public/resources/js/client.js
@@ -1,7 +1,16 @@
 window.onload = function(){
 	
 	
-	/*var demo_input = document.getElementById('demo_input');
+	
+	 editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+        mode: "text/html",
+        lineNumbers: true
+      });
+	
+	
+	
+	
+	var demo_input = document.getElementById('demo_input');
 	
 	var type_this = "<!DOCTYPE HTML><html>Hello, this is kinda the text sup?";
 	var index = 0;
@@ -9,16 +18,21 @@ window.onload = function(){
 	window.next_letter = function() {
 		if (index <= type_this.length) {
 			demo_input.value = type_this.substr(0, index++);
+			//editor.replaceRange(type_this.substr(0, index++), CodeMirror.Pos(editor.lastLine()))
+			editor.setValue(demo_input.value)
 			setTimeout("next_letter()", 20);
 		}
 	}
 	
-	next_letter();*/
+	next_letter();
 
- editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-        mode: "text/html",
-        lineNumbers: true
-      });
+
+
+
+
+
+
+
 	
 	
 }
