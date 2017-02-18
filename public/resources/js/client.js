c7950699725839764dd603301caebb5c31fe6d44
XXX <XXX>
2017-02-18 20:29:52 +0100

    XXX

---
+++ public/resources/js/client.js
@@ -0,0 +1,19 @@
+window.onload = function(){
+	
+	
+	var demo_input = document.getElementById('demo_input');
+	
+	var type_this = "<!DOCTYPE HTML><html>Hello, this is kinda the text sup?";
+	var index = 0;
+	
+	window.next_letter = function() {
+		if (index <= type_this.length) {
+			demo_input.value = type_this.substr(0, index++);
+			setTimeout("next_letter()", 20);
+		}
+	}
+	
+	next_letter();
+	
+	
+}
